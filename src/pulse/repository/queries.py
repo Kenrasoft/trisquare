749df24124926bcef5af8c70b55c8f0e61b24f0e
tejus-kenra <tejus17varma@gmail.com>
2023-09-17 19:31:00 -0400

    with query to fetch market_Cap data

--- src/pulse/repository/queries.py
+++ src/pulse/repository/queries.py
@@ -1,7 +1,8 @@
 from pulse.repository.database_connect import DatabaseConnect
 from pulse.repository.index_companies_repo import SP500_table
-from pulse.repository.stock_prices_repo import Daily_prices_table
+from pulse.repository.stock_prices_repo import Historical_prices_table
 from sqlalchemy import func
+import json
 
 # This class has all the different queries of different tables that can be used.
 class Queries:
@@ -41,15 +42,53 @@ class Queries:
         db_connector = DatabaseConnect()
         session = db_connector.connect_db()
         with session() as session:
-        # Query the SP500_table to get all symbols in the selected sector
             sector_companies = session.query(SP500_table.symbol).filter(SP500_table.sector == selected_sector).all()
             symbols = [company[0] for company in sector_companies]
+            
+            date_query = session.query(Historical_prices_table.date_time).distinct().all()
+            dates = [ d for d, in date_query]
+            
+            values=[]
+            
+            for d in dates:
+                total_marketcap = (
+                    session.query(func.sum(Historical_prices_table.market_cap))
+                    .filter(Historical_prices_table.symbol.in_(symbols))
+                    .where(Historical_prices_table.date_time == d)
+                    .scalar()
+                    )
+                total_volume = (
+                    session.query(func.sum(Historical_prices_table.volume))
+                    .filter(Historical_prices_table.symbol.in_(symbols))
+                    .where(Historical_prices_table.date_time == d)
+                    .scalar()
+                    )
 
-            # Calculate the total market cap for companies in the selected sector
-            total_marketcap = (
-                session.query(func.sum(Daily_prices_table.market_cap))
-                .filter(Daily_prices_table.symbol.in_(symbols))
-                .scalar()
-            )
+                values.append({ "date": d.strftime('%Y-%m-%d') , "volume": total_volume ,"marketcap" : total_marketcap })
 
-        return {"sector": selected_sector, "total_marketcap": total_marketcap}
+            return values
+        
+    def get_all_sector_marketcap(self):
+        db_connector = DatabaseConnect()
+        session = db_connector.connect_db()
+        with session() as session:
+            sectors = session.query(SP500_table.sector).distinct().all()
+            data = [sector for sector, in sectors]
+            data_for_sectors = []
+            for i in data:
+                
+                values = Queries().get_sector_marketcap(i)
+                output = {"sector":i, "historical":values}
+                data_for_sectors.append(output)
+
+            output_file = {"historicalSectors":data_for_sectors}
+            
+            file_path = './pulse/repository/sectors_data.json'  
+
+            with open(file_path, 'w') as file:
+                json.dump(output_file, file, indent=4)
+            
+            return
+            
+
+
